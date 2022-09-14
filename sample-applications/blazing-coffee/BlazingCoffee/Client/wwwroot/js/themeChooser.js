c5be62fd9024007b3b5aa286643de2a3895abbea
Joana <jivanova@progress.com>
2022-09-14 16:05:42 +0300

    chore(blazing-coffee): bump themes

--- sample-applications/blazing-coffee/BlazingCoffee/Client/wwwroot/js/themeChooser.js
+++ sample-applications/blazing-coffee/BlazingCoffee/Client/wwwroot/js/themeChooser.js
@@ -1,5 +1,5 @@
 ﻿window.themeChooser = {
-    themeVersion: "3.5.0",
+    themeVersion: "3.6.0",
     themeList: [
         { themeValue: "default-ocean-blue", mainTheme:"default", isSwatch: true },
         { themeValue: "default", mainTheme: "default", isSwatch: false },
