947ed8a58606ff9c4641a7c7eacc84ef39d2e7db
nckabob <nckabob@gmail.com>
2023-11-29 07:04:54 -0800

    TEST Commit

--- sample-applications/blazing-coffee/BlazingCoffee/Client/Shared/Themes/ThemeSetting.cs
+++ sample-applications/blazing-coffee/BlazingCoffee/Client/Shared/Themes/ThemeSetting.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BlazingCoffee.Client.Shared
+namespace Shared
 {
     public class ThemeSetting
     {
